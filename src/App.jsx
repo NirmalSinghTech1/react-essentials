8bb299e5eb694fe2caef53c675087875de8f6500
Nirmal Singh <nirmalsinghh007@gmail.com>
2026-02-10 18:51:24 +0530

    Added header and core concepts section with CSS Styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,62 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react-core-concepts.png";
+import componentLogo from "./assets/components.png";
+import jsxLogo from "./assets/jsx-ui.png";
+import propsLogo from "./assets/config.png";
+import stateLogo from "./assets/state-mgmt.png";
+import "./App.css";
+import { jsx } from "react/jsx-runtime";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+      <header>
+        <img src={reactLogo} alt="React logo" width={165} height={165} />
+        <h1>React Essentials</h1>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          Crucial React concepts you will need for almost any app you are going
+          to build!
         </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      </header>
+      <main>
+        <h2>Core Concepts</h2>
+        <ul id="core-concepts">
+          <li className="core-concept">
+            <img src={componentLogo} alt="React Component logo" />
+            <h3>Components</h3>
+            <p>
+              The core UI building block - compose the user interface by
+              combining multiple components.
+            </p>
+          </li>
+          <li className="core-concept">
+            <img src={jsxLogo} alt="JSX logo" />
+            <h3>JSX</h3>
+            <p>
+              Return (potentially dynamic) HTML(ish) code to define the actual
+              markup that will be rendered.
+            </p>
+          </li>
+          <li className="core-concept">
+            <img src={propsLogo} alt="Props logo" />
+            <h3>Props</h3>
+            <p>
+              Make components configurable (and therefore reusable) by passing
+              input data to them.
+            </p>
+          </li>
+          <li className="core-concept">
+            <img src={stateLogo} alt="State logo" />
+            <h3>State</h3>
+            <p>
+              React-managed data which, when changed, causes the component to
+              re-render & the UI to update.
+            </p>
+          </li>
+        </ul>
+      </main>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
